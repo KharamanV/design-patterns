f07d12f91098876e4911529aac0118d2a6db1692
Vladimir <kharaman.v@gmail.com>
2017-11-12 05:02:35 +0300

    Add Observer

--- observer.js
+++ observer.js
@@ -1,10 +1,73 @@
 // @flow
 
-/* Publisher */
-class Observable {
-  _listeners = [];
+class ListenersMap {
+  listeners = {};
 
-  subscribe(type, listener) {
-    this._listeners.push({ type, listener });
+  add(key, listener) {
+    if (!this.listeners[key]) {
+      this.listeners[key] = [];
+    }
+
+    this.listeners[key].push(listener);
+  }
+
+  get(eventType) {
+    return this.listeners[eventType] || [];
+  }
+
+  remove(key, listener) { /* remove listener */ }
+}
+
+/* Basic Publisher */
+class Observable {
+  _listeners = new ListenersMap();
+
+  subscribe(eventType, listener) {
+    this._listeners.add(eventType, listener);
+  }
+
+  unsubscribe(eventType, listener) {
+    this._listeners.remove(eventType, listener);
+  }
+
+  notify(eventType, value) {
+    this._listeners.get(eventType)
+      .forEach(listener => listener.update(value));
   }
 }
+
+class NewspaperPublisher extends Observable {
+
+  publishDailyProphet() {
+    const newspaper = {} // fresh newspaper
+
+    this.notify('DailyProphet', newspaper);
+  }
+
+  publishQuibbler() {
+    const newspaper = {} // fresh newspaper
+
+    this.notify('Quibbler', newspaper);
+  }
+}
+
+/* Subscriber interface */
+interface Observer {
+  update(value: any): void;
+}
+
+class Harry implements Observer {
+  update(newspaper) { /* read the newspaper */ }
+}
+
+class Ronald implements Observer {
+  update(newspaper) { /* read the newspaper */ }
+}
+
+const publisher = new NewspaperPublisher();
+
+publisher.subscribe('DailyProphet', new Harry());
+publisher.subscribe('Quibbler', new Ronald());
+
+publisher.publishDailyProphet();
+publisher.publishQuibbler();
